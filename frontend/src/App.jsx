a9594192378702e46be9274b64d1d9a9c386dfa2
Francis <frluemdemu@gmail.com>
2025-03-04 18:45:10 +0800

    Started frontend, made sidebar logo and company name layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Inventory from './pages/inventory';
+import Process from './pages/process';
+import Records from './pages/records';
+import placeHolder from './assets/image-placeholder.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activePage, setActivePage] = useState("process");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="sidebar">
+        <img src={placeHolder} alt="Sidebar Logo"/>
+        <h1>Porky Best!</h1>
+        <div className="sideButtonContainer">
+          <button className="sideButton" onClick={() => setActivePage("process")}>Process</button>
+          <button className="sideButton" onClick={() => setActivePage("records")}>Records</button>
+          <button className="sideButton" onClick={() => setActivePage("inventory")}>Inventory</button>
+        </div>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
