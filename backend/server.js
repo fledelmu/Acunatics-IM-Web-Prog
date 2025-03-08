c5e91071737cd5b1dc45ebbebebcf0629e5fe063
Allain <xdallain@gmail.com>
2025-03-08 20:25:23 +0800

    connected cloud db

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,22 @@ import cors from "cors";
 import db from "./db.js";
 
 const app = express();
-const PORT = process.env.DB_PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// Test route to fetch data
+//test ra ni if mag kuha siya sa cloud na database
 app.get("/users", async (req, res) => {
   try {
-    const [users] = await db.query("SELECT * FROM tbl_pet"); 
+    const [users] = await db.query("SELECT * FROM sales"); 
     res.json(users);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+});
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
 });
 
 app.listen(PORT, () => {
