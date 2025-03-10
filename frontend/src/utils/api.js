7af475f4fda497c24c6808566ac04ccf59f01845
Francis <frluemdemu@gmail.com>
2025-03-10 17:53:03 +0800

    Test

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,3 +1,14 @@
 import axios from 'axios'
 import {useState, useEffect} from 'react'
 
+const URL = "http://localhost:5000"
+
+export const getProduction = async () =>{
+    try{
+        const response = await axios.get(`${URL}/api/production-records`)
+        return response.data
+    } catch(error) {
+        console.error("Error fetching records:", error);
+        return []
+    }
+}
